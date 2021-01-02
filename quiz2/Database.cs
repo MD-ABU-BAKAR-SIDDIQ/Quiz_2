963dc1b06814657588f73dc5501443fad6346e8d
unknown <siddiqarman66@gmail.com>
2021-01-02 23:01:17 +0600

    quiz-2

---
+++ quiz2/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz2
+{
+    class Database
+    {
+        public static SqlConnection ConnectDB()
+        {
+            string connString = "data source = DESKTOP-CPV8G9B\\SQLEXPRESS; database= Library; integrated security = True";
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
